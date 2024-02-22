1dba29ed766f8eb82a3fbfb06115df429803de3c
Juan Santos <juan.santos@texascontrols.com.com>
2024-02-22 13:32:16 +0100

    Uso de las directivas async and await para la obtención de datos a partir de un sevcio de mocks remoto.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,26 @@ class Loading extends StatefulWidget {
 }
 
 class LoadingState extends State<Loading> {
+
+  void getData() async {
+    
+    Uri uri = Uri(
+      scheme: 'https',
+      host: 'jsonplaceholder.typicode.com',
+      path: 'todos/1',
+    );
+    Response response = await get(uri);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
