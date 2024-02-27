ed31c7ccd4d4404d54becb725bee4866df3ff2d0
Juan Santos <juan.santos@texascontrols.com.com>
2024-02-27 17:08:29 +0100

    Añadido un ListView con la lista de ciudades para poder ver su hora

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/location.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +8,18 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class ChooseLocationState extends State<ChooseLocation> {
+
+  List<Location> locations = [
+      Location(continent: 'Europe', city: 'London', flag: 'uk.png'),
+      Location(continent: 'Europe', city: 'Athens', flag: 'greece.png'),
+      Location(continent: 'Africa', city: 'Cairo', flag: 'egypt.png'),
+      Location(continent: 'Africa', city: 'Nairobi', flag: 'kenya.png'),
+      Location(continent: 'America', city: 'Chicago', flag: 'usa.png'),
+      Location(continent: 'America', city: 'New York', flag: 'usa.png'),
+      Location(continent: 'Asia', city: 'Seoul', flag: 'south_korea.png'),
+      Location(continent: 'Asia', city: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +35,25 @@ class ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen'),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].city);
+                },
+                title: Text(locations[index].city),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
